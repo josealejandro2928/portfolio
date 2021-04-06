ac0b004af960f670b66e5094cbaa437b357d0391
Jose Alejandro Concepcion Alvarez <jalejandro2928@gmail.com>
2021-04-06 17:40:26 -0500

    Auto-generated commit

---
+++ portafolio/assets/db/testimonials.js
@@ -0,0 +1,84 @@
+let data = [
+  {
+    name: "Héctor Alonso Remedios",
+    text: `He tenido el placer de trabajar junto a este gran profesional. Lo considero un crack!!
+    Presenta muy buenas habilidades, tanto hard como soft skills. `,
+    profession: "Python Developer",
+    socials: [
+      {
+        class: "fab fa-linkedin",
+        url: "https://www.linkedin.com/mwlite/in/hectorarem",
+      },
+    ],
+  },
+  {
+    name: "Alejandro Ravelo Julian",
+    text: `Jose Alejandro Concepción is an excellent worker, has exceptional analytical and development skills.
+     He has a high knowledge of software architecture and programming languages.
+     I have worked directly with him on highly relevant projects and have demonstrated his commitment to work,
+     honesty, camaraderie and problem-solving ability. He possesses a great capacity for learning and adaptation
+     to complex work environments. I recommend Jose Alejandro Concepción as an excellent worker
+     and a good acquisition for any company.`,
+    profession: "Frontend Developer at GuajiTech",
+    socials: [
+      {
+        class: "fab fa-linkedin",
+        url: "https://www.linkedin.com/in/alejandro-ravelo-julian/",
+      },
+    ],
+  },
+  {
+    name: "Yoilan Rios Rosales",
+    text: `Gran trabajador, muy dedicado, trata sus proyectos con el amor de una madre por sus hijos.
+     Definitivamente el trabajador que se necesita para impulsar un proyecto.`,
+    profession: "Backend developer",
+    socials: [
+      {
+        class: "fab fa-linkedin",
+        url: "https://www.linkedin.com/in/yoilan-rios-rosales-94a42b179/",
+      },
+    ],
+  },
+  {
+    name: "Michel Reynaldo Brito",
+    text: `This is an incredible professional, dedicated to work with a great capacity for assimilation
+    of new technologies and good practices in web development, a pleasure to work with him.`,
+    profession: "Font End Developer at GuajiTech",
+    socials: [
+      {
+        class: "fab fa-linkedin",
+        url:
+          "https://www.linkedin.com/in/michelreynaldobrito-automation-development/",
+      },
+    ],
+  },
+  {
+    name: "Enrique Plasencia",
+    text: `Es uno de los mejores desarrolladores con los que he trabajado.
+     y responsable como el mejor. Experto en Angular, Css, Javascript, Node JS.`,
+    profession: "Lead front-end developers en GuajiTech",
+    socials: [
+      {
+        class: "fab fa-linkedin",
+        url:
+          "https://www.linkedin.com/in/enrique-plasencia-62a6aa1a5/",
+      },
+    ],
+  },
+  {
+    name: "Carlos Alberto Villanueva Hedesa",
+    text: `Hemos trabajado juntos en varios proyectos y la profesionalidad se nota al concluir un proyecto.
+    La entrega que le pone a cada trabajo que realiza es de admirar.
+    Recomendado a máximo para desarrollo de frontend y backend.`,
+    profession: "Back End Developer at GuajiTech",
+    socials: [
+      {
+        class: "fab fa-linkedin",
+        url:
+          "https://www.linkedin.com/in/carlos-alberto-villanueva-hedesa-058685144/",
+      },
+    ],
+  },
+];
+
+module.exports = data;
