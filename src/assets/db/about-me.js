b2e846799f2debfc2005b87b919306630b15b165
Jose Alejandro Concepcion Alvarez <jalejandro2928@gmail.com>
2021-04-07 23:56:26 -0500

    Corregido problemas de redaccion

--- src/assets/db/about-me.js
+++ src/assets/db/about-me.js
@@ -26,22 +26,22 @@ let data = {
   resumeText: {
     en: `
     <p>
-      Graduated from a degree in Automation Engineering. I work as a full stack developer using technologies based on the Javascript
+      Graduated from a degree in Automation Engineering. I work as a  <strong>Full Stack developer </strong> using technologies based on the  <strong>Javascript </strong>
       programming language. I have also researched in the field of Robotics, leading me to develop projects such as an <strong>Ackermarn Drive Robot</strong>
       with smart features like obstacle avoidance and path planning, as well as building a two wheel <strong> Self-Balancing Robot </strong> that was
-      my thesis work. I participated in the ACM International Collegiate Programming Competition (ICPC) several times,
+      my thesis work. I participated in the  <strong>ACM International Collegiate Programming Competition (ICPC) </strong> several times,
       and my team managed to classify to the regional finals two times. As a result, I acquired knowledge about data structures
       and efficient algorithm design. In addition, I have  experience developing mobile and web application using frameworks
       such as <strong>Angular, React, Nodejs, Express, Flask e Ionic </strong>.
      </p>`,
     es: `
     <p>
-      Egresado de la carrera de Ingeniería en Automatización. Trabajo como desarrollador full stack usando tecnologías basadas en Javascript.
+      Graduado de la carrera de Ingeniería en Automatización. Trabajo como desarrollador  <strong>Full Stack </strong> usando tecnologías basadas en  <strong>Javascript </strong>.
       También he investigado en el campo de la Robótica, lo que me ha llevado al desarrollo de proyectos como: El diseño de un
       <strong>Robot con Accionamiento Ackermarn </strong> con tareas como: La evitación de obstáculos y la planificación de trayectorias.
-      También implemente un <strong>Robot Autobalanceado de dos Ruedas</strong> en mi trabajo de tesis. Participé en el Concurso Internacional de Programación Colegiada ACM (ICPC) varias veces,
-      y mi equipo logró clasificar a la final regional en dos ocasiones. Como resultado, adquirí conocimientos sobre estructuras de datos.
-      y diseño de algoritmos eficiente. Además, tengo experiencia en el desarrollo de aplicaciones web y móviles utilizando frameworks.
+      También desarrollé un <strong>Robot Autobalanceado de dos Ruedas</strong> en mi trabajo de tesis. Participé en el Concurso Internacional de Programación Colegiada  <strong>ACM (ICPC) </strong> varias veces,
+      y mi equipo logró clasificar a la final regional en dos ocasiones. Como resultado, adquirí conocimientos sobre  <strong>estructuras de datos y
+      diseño de algoritmos eficientes </strong>. Además, tengo experiencia en el desarrollo de aplicaciones web y móviles utilizando frameworks.
       como <strong>Angular, React, Nodejs, Express, Flask e Ionic </strong>.
     </p>`,
   },
